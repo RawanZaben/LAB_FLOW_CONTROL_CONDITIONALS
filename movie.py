3e0f64d5aff9e450bb396b5f50060520a7818103
RawanZaben <rawan.zaben.rae.js@tuwaiq.edu.sa>
2022-02-20 16:32:34 +0300

    lab 2

--- movie.py
+++ movie.py
@@ -8,7 +8,7 @@ if rating >= 4 and popularity > 80:
 elif rating >= 3 and popularity > 70:
     print("I recommended it . It is good")
 
-elif rating <= 2 and popularity > 70:
+elif rating <= 2 and popularity > 60:
     print("You should check it out!")
 
 else:
